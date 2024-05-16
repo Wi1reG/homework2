297886b9820dbf2157b19ee1ab0662f34e40a3e0
Wi1reG <anikin-andrey1@mail.ru>
2024-05-16 14:50:02 +0300

    task 2

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,11 @@ public class Main {
 
     public static void main(String[] args) {
 
-        var dog = 8.0;
+        var dog = 8.0 + 4;
         System.out.println("god" + dog);
-        var cat = 3.6;
+        var cat = 3.6 + 4;
         System.out.println("cat" + cat);
-        var paper = 763789;
+        var paper = 763789 + 4;
         System.out.println("paper" + paper);
 
     }
